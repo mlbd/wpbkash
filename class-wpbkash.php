663c5a526b0680fff616a3ea6a3708f959bf4b93
mlbd <mlimonbd@gmail.com>
2021-02-14 14:23:44 +0600

    change file name

--- class-wpbkash.php
+++ class-wpbkash.php
@@ -3,7 +3,7 @@
  * Plugin Name:       bKash WordPress Payment
  * Plugin URI:        https://wordpress.org/plugins/wpbkash/
  * Description:       bKash payment gateway integration for WordPress
- * Version:           0.1.7
+ * Version:           0.1.8
  * Author:            themepaw
  * Author URI:        https://themepaw.com
  * Text Domain:       wpbkash
@@ -29,7 +29,7 @@ final class WPbKash {
 	 *
 	 * @var string
 	 */
-	const VERSION = '0.1.7';
+	const VERSION = '0.1.8';
 
 	/**
 	 * Plugin Database Table.
